6344c035e3209e0df5e3b1c7264aa9166b51f074
sedovev <sedovev@rgn.gov.ru>
2025-05-25 20:59:02 +0700

    Добавлен компонент SectionDivider и его использование на главной странице

---
+++ src/components/organisms/sections/SectionDivider.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="section-divider">
+    <div class="container">
+      <div class="section-divider__title-wrapper">
+        <div class="section-divider__line"></div>
+        <h2 class="section-divider__title">{{ title }}</h2>
+      </div>
+      <p class="section-divider__text">{{ text }}</p>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  text: string;
+}>();
+</script>
+
+<style scoped>
+.section-divider {
+  background-color: #ffd13d;
+
+  width: 100%;
+  padding-top: 60px;
+  padding-bottom: 60px;
+}
+
+.section-divider__title-wrapper {
+  position: relative;
+  margin: 0 auto;
+
+  margin-bottom: 60px;
+  width: fit-content;
+  text-align: center;
+}
+
+.section-divider__line {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 55%;
+  height: 14px;
+  background-color: white;
+  z-index: 1;
+  transform: translateY(-50%);
+}
+
+.section-divider__title {
+  color: #14213d;
+
+  font-size: 84px;
+  line-height: 90px;
+  font-weight: 900;
+  text-align: center;
+
+  margin: 0 4px;
+  position: relative;
+  z-index: 2;
+  display: inline-block;
+}
+
+.section-divider__text {
+  color: #14213d;
+  font-size: 20px;
+
+  line-height: 24px;
+  font-weight: bold;
+  text-align: center;
+  margin: 0 auto;
+  max-width: 800px;
+}
+</style>
--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-import PageLayout from '@/components/templates/PageLayout.vue'
+import PageLayout from '@/components/templates/PageLayout.vue';
 import Hero from '../organisms/sections/Hero.vue';
+import SectionDivider from '@/components/organisms/sections/SectionDivider.vue';
 </script>
 
 <template>
   <PageLayout>
-    
+    <SectionDivider
+      title="Продукция"
+      text="Снабжение строительных объектов расходными материалами"
+    />
   </PageLayout>
 </template>
